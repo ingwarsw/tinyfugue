8de96db9fd45d4ff8e9afee86aa49ace296f8e6c
Will <github@will.systems>
2018-03-01 23:53:00 -0600

    ifdef for openssl >= 1.0.2

--- src/socket.c
+++ src/socket.c
@@ -1395,13 +1395,12 @@ static int opensock(World *world, int flags)
 	ssl_options.verbose_mode = ssl_verbose;
 	ssl_options.always_continue = ssl_continue;
 	SSL_set_ex_data(xsock->ssl, ssl_mydata_index, &ssl_options);
+#if OPENSSL_VERSION_NUMBER >= 0x1000200fL
 	X509_VERIFY_PARAM *param = SSL_get0_param(xsock->ssl);
-#ifndef X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS
-# define X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS 0
-#endif
 	X509_VERIFY_PARAM_set_hostflags(param, 
 		X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
 	X509_VERIFY_PARAM_set1_host(param, xsock->world->host, 0);
+#endif
 #else
         CONFAIL(xsock, "ssl", "not supported");
 #endif
